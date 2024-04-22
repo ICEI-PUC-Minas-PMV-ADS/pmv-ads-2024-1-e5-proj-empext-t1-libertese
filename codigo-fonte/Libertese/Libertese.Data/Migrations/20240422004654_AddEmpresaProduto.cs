91f40cc4d41864988a0f1b651b59220faab436fb
thatavieira <thagurgel0909@gmail.com>
2024-04-21 22:22:10 -0300

    adicionando migration de empresa

---
+++ codigo-fonte/Libertese/Libertese.Data/Migrations/20240422004654_AddEmpresaProduto.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Libertese.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmpresaProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Empresa",
+                table: "Produtos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Preco",
+                table: "Produtos",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Empresa",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Preco",
+                table: "Produtos");
+        }
+    }
+}
