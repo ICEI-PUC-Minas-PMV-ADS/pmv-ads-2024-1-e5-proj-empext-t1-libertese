cfe03ce470ecd8c001039fefb94e286b78d1b4a5
rafasensacional <rafadoteclado@gmail.com>
2024-05-06 15:31:51 -0300

    Classificacoes-e-forma-de-pagamento

---
+++ codigo-fonte/Libertese/Libertese.Domain/Enums/FormaDePagamento.cs
@@ -0,0 +1,14 @@
+﻿namespace Libertese.Domain.Enums
+{
+    public enum FormaPagamentoEnum
+    {
+        Boleto = 1,
+        Cheque = 2,
+        CreditoPrazo = 3,
+        CreditoVista = 4,
+        Debito = 5,
+        Dinheiro = 6,
+        Pix = 7,
+        Transferencia = 8
+    }
+}
