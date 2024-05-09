2bac66a379cee1b37b1116c20f686d306ee97366
Douglas Delarett <douglasdelareti@hotmail.com>
2024-05-08 21:04:10 -0300

    Validacao de erros de formatação

--- codigo-fonte/Libertese/Libertese.Domain/Entities/Cadastro/Funcionario.cs
+++ codigo-fonte/Libertese/Libertese.Domain/Entities/Cadastro/Funcionario.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Libertese.Domain.Entities.Cadastro
 {
@@ -15,12 +16,16 @@ namespace Libertese.Domain.Entities.Cadastro
 
         public required decimal Salario { get; set; }
 
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}\-\d{2}$", ErrorMessage = "Por favor, insira um CPF válido no formato XXX.XXX.XXX-XX.")]
         public required string Cpf {  get; set; }
 
         public required string Sexo { get; set; }
 
         public required string Email { get; set; }
 
+        [Required(ErrorMessage = "O número de celular é obrigatório.")]
+        [RegularExpression(@"^(\d{0,2})(\d{0,5})(\d{0,4})$", ErrorMessage = "Por favor, insira um número de celular válido no formato (XX) XXXXX-XXXX")]
         public required string Celular { get; set; }
 
         public required string Function { get; set; }
