48065aad444c2bc591b4f82887766b2afccfd458
Thata Vieira <thagurgel0909@gmail.com>
2024-05-09 18:41:47 -0300

    Criacao e configuracao do projeto de teste da aplicacao

---
+++ codigo-fonte/Libertese/Libertese.Test/ClassificacaoControllerTest.cs
@@ -0,0 +1,16 @@
+namespace Libertese.Test
+{
+    public class ClassificacaoControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ codigo-fonte/Libertese/Libertese.Test/ClassificacaoRepositoryTest.cs
@@ -0,0 +1,16 @@
+namespace Libertese.Test
+{
+    public class ClassificacaoRepositoryTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
