51d42ed4f447716d4f59998944f9740cff1ea325
kahmassensini <karinneamp22@gmail.com>
2024-05-28 22:05:59 -0300

    .

--- codigo-fonte/Libertese/Libertese.Domain/Entities/Financeiro/Cliente.cs
+++ codigo-fonte/Libertese/Libertese.Domain/Entities/Financeiro/Cliente.cs
@@ -9,12 +9,10 @@ namespace Libertese.Domain.Entities.Financeiro
     public class Cliente : BaseEntity
     {
         public required string Nome { get; set; }
-        public string? Endereco { get; set; }
         public string? Cpf { get; set; }
         public string? Cnpj { get; set; }
         public string? Telefone { get; set; }
         public string? Email { get; set; }
-        public string? DadosBancarios { get; set; }
 
     }
 }
