4f49a70a73c5f293712b669e526d0b88c94726a9
kahmassensini <karinneamp22@gmail.com>
2024-05-30 00:32:00 -0300

    Ajuste de create Receita e inicio Cliente

---
+++ codigo-fonte/Libertese/Libertese.Data/Migrations/20240529012415_AtualizarCliente.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Libertese.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizarCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DadosBancarios",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Endereco",
+                table: "Clientes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Receitas",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Receitas",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DadosBancarios",
+                table: "Clientes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Endereco",
+                table: "Clientes",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
