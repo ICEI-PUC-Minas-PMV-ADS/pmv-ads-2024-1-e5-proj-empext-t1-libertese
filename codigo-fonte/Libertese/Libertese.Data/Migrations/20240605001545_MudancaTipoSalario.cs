9f98a38e5f751f54035ddc652e5af83b779a8808
Douglas Delarett <douglasdelareti@hotmail.com>
2024-06-05 21:54:57 -0300

    Mudança no banco de dados para novos campos e mudanca do tipo do Salario para adicionar o r$

---
+++ codigo-fonte/Libertese/Libertese.Data/Migrations/20240605001545_MudancaTipoSalario.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Libertese.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MudancaTipoSalario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Salario",
+                table: "Funcionarios",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CpfCnpj",
+                table: "Clientes");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Salario",
+                table: "Funcionarios",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Clientes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cnpj",
+                table: "Clientes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+    }
+}
